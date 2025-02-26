91941fec9fd9835d203e0239f80afd6edfa97dcc
Hemant Shukla <hbshukla@gmail.com>
2025-02-26 23:06:24 +0530

    DataType Demo

---
+++ src/day2/DataTypeDemo.java
@@ -0,0 +1,20 @@
+package day2;
+
+public class DataTypeDemo {
+
+	public static void main(String[] args) 
+	{
+	//byte -128 to 127
+	
+		byte by=125;
+		System.out.println(by);
+		
+		short sh=3535; //-32768 to 32767
+		System.out.println(sh);
+		
+		long l=12345678912345l; //-9223372036854775808  to 9223372036854775807
+		System.out.println(l);
+
+	}
+
+}
--- src/day2/VariableDemo.java
+++ src/day2/VariableDemo.java
@@ -4,8 +4,29 @@ public class VariableDemo {
 
 	public static void main(String[] args) 
 	{
-		
+		int i =1; //declaration + assignment 
+		System.out.println(i+1);
 
+		i = 100;
+		System.out.println(i+1);
+		
+		//Approach-1
+		/*int a =100;
+		int b =200;
+		int c =300;*/
+		
+		//Approach-2
+		/*int a,b,c; //Declaration
+		int a =100; //Assignment
+		int b =200;
+		int c =300;*/
+		
+		//Approach-3
+		int a=100,b=200,c=300;
+		System.out.println("The value of a is:"+a);
+		System.out.println(b);
+		System.out.println(""+a+b+c);
+		System.out.println(a+","+b+","+c);
 	}
 
 }
