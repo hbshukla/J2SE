19bcb4e0561c8e05d78c4547df3bf5e00b2fb655
Hemant Shukla <hbshukla@gmail.com>
2025-02-27 17:59:45 +0530

    DataTypeDemo

--- src/day2/DataTypeDemo.java
+++ src/day2/DataTypeDemo.java
@@ -13,8 +13,29 @@ public class DataTypeDemo {
 		System.out.println(sh);
 		
 		long l=12345678912345l; //-9223372036854775808  to 9223372036854775807
-		System.out.println(l);
+		System.out.println(l); //literal is needed
+		
+		//Decimal Number - float - double
+		
+		float item_price=15.5f;
+		System.out.println(item_price);//literal is needed
+		
+		double dbl=1234.4321;
+		System.out.println(dbl);
+		
+		char grad = 'A';
+		System.out.println(grad);
 
-	}
-
+		//==================
+		String name = "Hemant";
+		//String name = 'Hemant' //Invalid
+		System.out.println(name);
+		
+		boolean b1=true;
+		System.out.println(b1);
+		
+		String c1="true";
+		System.out.println(c1);
+						
+}
 }
