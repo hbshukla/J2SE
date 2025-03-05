c881384d954cf1eaffccee1cdae82068943c773d
Hemant Shukla <hbshukla@gmail.com>
2025-03-05 19:22:34 +0530

    "Day-4- if, if-else, nested if-else Done"

---
+++ src/Day4/EvenOrOddNumber.java
@@ -0,0 +1,16 @@
+package Day4;
+
+public class EvenOrOddNumber {
+
+	public static void main(String[] args) { 
+	int num=11;
+	if (num%2==0)
+	{
+		System.out.println("Even Number");
+	}
+	else
+	{
+		System.out.println("Odd Number");
+	}
+}
+}
---
+++ src/Day4/PositiveOrNegetiveNumber.java
@@ -0,0 +1,23 @@
+package Day4;
+
+public class PositiveOrNegetiveNumber {
+
+	public static void main(String[] args) {
+		
+		int num=1;
+		if(num>0)
+		{
+			System.out.println("Positive Number");
+		}
+		else if(num<0)
+		{
+			System.out.println("Negative Number");
+		}
+		else
+		{
+			System.out.println("Zero");
+		}
+
+	}
+
+}
