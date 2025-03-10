c603d8fe5cd923dcb4b893d60c492f2ffd665d04
Hemant Shukla <hbshukla@gmail.com>
2025-03-10 19:48:11 +0530

    Day-5 Do While - For Loop

---
+++ src/day5/WhileVsDoWhile.java
@@ -0,0 +1,26 @@
+package day5;
+
+public class WhileVsDoWhile {
+
+	public static void main(String[] args) 
+	{
+	
+		int i =1;
+		while(i<=5)
+		{
+			System.out.println(i);
+			i++;
+		}
+		
+		System.out.println("//Do While Loop");
+	
+		int a=1;
+		do
+		{
+			
+			System.out.println(a);
+			a++;
+		}while(a<=5);
+	}
+
+}
