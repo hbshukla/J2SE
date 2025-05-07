30b076e91ced60cc2db9ce6cd7e8918f9c5ccbad
Hemant Shukla <hbshukla@gmail.com>
2025-05-07 19:08:07 +0530

    practise

--- src/Assignment/PrimeNumber.java
+++ src/Assignment/PrimeNumber.java
@@ -12,7 +12,7 @@ public class PrimeNumber {
 int count=0;
 		for (int n = 2; n <= 100; n++) {
 			boolean isPrime = true;
-			for (int i = 2; i < n ; i++) {
+			for (int i = 2; i <=Math.sqrt(n); i++) {
 
 				if (n % i == 0) {
 					isPrime = false;
---
+++ src/Assignment/Temp_asn.java
@@ -0,0 +1,25 @@
+package Assignment;
+
+public class Temp_asn {
+	public static void main(String[] args) {
+		int n = 15;
+
+		{
+			for (int i = 2; i <= n; i++) {
+				boolean isPrime=true;;
+				{
+					for (int j = 2; j <= Math.sqrt(i); j++) {
+						if (i % j == 0) {
+							isPrime = false;
+							break;
+						}
+					}
+					if (isPrime) {
+						System.out.println("Prime Numbers are :" + i);
+					}
+				}
+
+			}
+		}
+	}
+}
